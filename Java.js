62eab4b48ae946e63ad7766176ec2bdb4bd18ca5
Ben Brandon/ <88277718+BenBrandon97@users.noreply.github.com>
2022-07-03 16:21:28 +0300

    Update Java.js

--- Java.js
+++ Java.js
@@ -9,7 +9,7 @@ function thanksforGiving(){
   let name = document.getElementById("name").value;
   console.log("Your name is " + name);
 // get the location from textbox
-  let area = document.getElementById("where").value;
+  let where = document.getElementById("where").value;
   console.log("The Lord bless " + where + " as a whole");
 //display msg
 document.getElementById("msg").textContent = "God bless the work of your hands Mr. " + name + " and remember to give to another with the same heart.";
