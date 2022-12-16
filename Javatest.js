81c902ce948862ef12b1ae792dadf97106bcdd83
Ben Brandon/ <benbrandon97@gmail.com>
2022-12-16 17:18:48 +0300

    Update Javatest.js

--- Javatest.js
+++ Javatest.js
@@ -1,1 +1,18 @@
+//This is the command used to write into HTML file
+document.write("Hello, World of JavaScript");
+
+// Output to the browser
+console.log("Hello Java language");
+
+//Variables... use the word var to declare variables
+var x = 10;
+var my_variable = 32;
+//var _name, $ben; This format can also be followed in variable declaration.
+
+//Single line comment
+alert("An alert box");
+
+/* This is a
+multiple line
+comment */
 
